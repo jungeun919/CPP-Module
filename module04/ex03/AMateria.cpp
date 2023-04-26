222d7c97382419f7193fe8cac277ebd094836a80
jungeun919 <renee8369@likelion.org>
2023-04-26 21:11:01 +0900

    module04 주석 제거

--- module04/ex03/AMateria.cpp
+++ module04/ex03/AMateria.cpp
@@ -1,7 +1,5 @@
 #include "AMateria.hpp"
 
-// type 변수를 참조로 전달 (객체의 복사본이 아닌 원본 전달)
-// 메모리와 시간 절약
 AMateria::AMateria(std::string const & type)
 {
 	this->type = type;
@@ -27,9 +25,6 @@ AMateria&	AMateria::operator=(const AMateria& object)
 	return *this;
 }
 
-// 반환값 (type) 변경 x
-// 해당 객체의 모든 멤버 변수를 RDONLY
-// 코드 수정 방지
 std::string const & AMateria::getType() const
 {
 	return this->type;
@@ -37,5 +32,6 @@ std::string const & AMateria::getType() const
 
 void	AMateria::use(ICharacter& target)
 {
+	(void)target;
 	std::cout << "this function will not be called" << std::endl;
 }
--- module04/ex03/main.cpp
+++ module04/ex03/main.cpp
@@ -5,137 +5,33 @@
 
 int	main()
 {
-	// {
-	// 	AMateria *ice1 = new Ice();
-	// 	AMateria *ice1_clone = ice1->clone();
-	// 	std::cout << "ice1_clone type: " << ice1_clone->getType() << std::endl;
+	IMateriaSource *src = new MateriaSource();
 
-	// 	ice1->use();
-	// 	ice1_clone->use();
+	src->learnMateria(new Ice());
+	src->learnMateria(new Cure());
 
-	// 	std::cout << std::endl;
-	// }
-	{
-		IMateriaSource *src = new MateriaSource();
+	std::cout << std::endl;
 
-		src->learnMateria(new Ice());
-		src->learnMateria(new Cure());
+	ICharacter *me = new Character("me");
 
-		std::cout << std::endl;
+	AMateria *tmp;
+	tmp = src->createMateria("ice");
+	me->equip(tmp);
+	tmp = src->createMateria("cure");
+	me->equip(tmp);
 
-		ICharacter *me = new Character("me"); // slot (4)
+	std::cout << std::endl;
 
-		AMateria *tmp;
-		tmp = src->createMateria("ice");
-		me->equip(tmp); // save slot
-		tmp = src->createMateria("cure");
-		me->equip(tmp);
+	ICharacter *bob = new Character("bob");
 
-		std::cout << std::endl;
+	me->use(0, *bob);
+	me->use(1, *bob);
 
-		ICharacter *bob = new Character("bob"); // target
+	delete bob;
+	delete me;
+	delete src;
 
-		me->use(0, *bob);
-		me->use(1, *bob);
+	system("leaks a.out");
 
-		delete bob;
-		delete me;
-		delete src;
-	}
-	
-	// {
-	// 	std::cout << std::endl;
-	// 	std::cout << "CONSTRUCTORS:" << std::endl;
-	// 	std::cout << "-----------------------" << std::endl;
-	// 	IMateriaSource *src = new MateriaSource();
-	// 	src->learnMateria(new Ice());
-	// 	src->learnMateria(new Cure());
-	// 	ICharacter *me = new Character("me");
-	// 	std::cout << std::endl;
-
-	// 	std::cout << "CREATE MATERIA:" << std::endl;
-	// 	std::cout << "-----------------------" << std::endl;
-	// 	AMateria *tmp;
-
-	// 	AMateria *tmp1;
-	// 	AMateria *tmp2;
-	// 	AMateria *tmp3;
-	// 	const AMateria *tmp4;
-
-	// 	tmp = src->createMateria("ice");
-	// 	me->equip(tmp);
-	// 	tmp1 = src->createMateria("cure");
-	// 	me->equip(tmp1);
-	// 	tmp = src->createMateria("fire");
-	// 	me->equip(tmp);
-	// 	std::cout << std::endl;
-
-	// 	std::cout << "USE ON A NEW CHARACTER:" << std::endl;
-	// 	std::cout << "-----------------------" << std::endl;
-	// 	ICharacter *bob = new Character("bob");
-	// 	me->use(0, *bob);
-	// 	me->use(1, *bob);
-	// 	std::cout << std::endl;
-	// 	me->use(2, *bob);
-	// 	me->use(-4, *bob);
-	// 	me->use(18, *bob);
-	// 	std::cout << std::endl;
-
-	// 	std::cout << "DEEP COPY CHARACTER:" << std::endl;
-	// 	std::cout << "-----------------------" << std::endl;
-	// 	Character *charles = new Character("Charles");
-	// 	tmp2 = src->createMateria("cure");
-	// 	charles->equip(tmp2);
-	// 	tmp3 = src->createMateria("ice");
-	// 	charles->equip(tmp3);
-	// 	tmp = src->createMateria("earth");
-	// 	charles->equip(tmp);
-	// 	Character *charles_copy = new Character(*charles);
-	// 	std::cout << std::endl;
-
-	// 	std::cout << "DEEP COPY VS SOURCE:" << std::endl;
-	// 	std::cout << "-----------------------" << std::endl;
-	// 	charles->unequip(0);
-	// 	tmp4 = charles_copy->getMateriaByIdx(1);
-	// 	charles_copy->unequip(1);
-	// 	delete tmp4;
-	// 	tmp = src->createMateria("cure");
-	// 	charles_copy->equip(tmp);
-	// 	tmp = src->createMateria("ice");
-	// 	charles_copy->equip(tmp);
-	// 	std::cout << std::endl;
-
-	// 	charles->use(0, *bob);
-	// 	charles->use(1, *bob);
-	// 	charles->use(2, *bob);
-	// 	charles->use(3, *bob);
-	// 	std::cout << std::endl;
-	// 	charles_copy->use(0, *bob);
-	// 	charles_copy->use(1, *bob);
-	// 	charles_copy->use(2, *bob);
-	// 	charles_copy->use(3, *bob);
-	// 	std::cout << std::endl;
-
-	// 	std::cout << "UNEQUIP:" << std::endl;
-	// 	std::cout << "-----------------------" << std::endl;
-	// 	me->unequip(-1);
-	// 	me->unequip(18);
-	// 	me->unequip(3);
-	// 	std::cout << std::endl;
-	// 	me->use(1, *charles);
-	// 	me->unequip(1);
-	// 	me->use(1, *charles);
-	// 	std::cout << std::endl;
-
-	// 	std::cout << "DESTRUCTORS:" << std::endl;
-	// 	std::cout << "-----------------------" << std::endl;
-	// 	delete bob;
-	// 	delete me;
-	// 	delete src;
-	// 	delete charles;
-	// 	delete charles_copy;
-	// 	delete tmp1;
-	// 	delete tmp2;
-	// 	std::cout << std::endl;
-	// }
+	return 0;
 }
